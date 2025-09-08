f90e7ad70d4699a6813453fd97d59f4d58959e74
LEECH3 <leech3@oocl.com>
2025-09-08 11:23:08 +0800

    Feat: add the Klass class

---
+++ src/main/java/oo/Klass.java
@@ -0,0 +1,31 @@
+package oo;
+
+import java.util.Objects;
+
+public class Klass {
+    private Integer number;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return Objects.equals(number, klass.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
+}
